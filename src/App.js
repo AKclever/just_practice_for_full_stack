fb4d8c01f311a98b850b8f6c57aee3d6d0ee8895
AKclever <aleksiskrass@gmail.com>
2023-11-23 16:22:43 +0200

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import ProductListing from './components/ProductListing';
+
+import EcommerceNavbar from './components/EcommerceNavbar';
+import EcommerceFooter from './components/EcommerceFooter';
+import EcommerceHeader from './components/EcommerceHeader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [products, setProducts] = useState([]);
+ const [error, setError] = useState(null);
+ const [isLoading, setIsLoading] = useState(false);
+
+ const fetchData = async () => {
+  setIsLoading(true);
+  try {
+    const response = await fetch('http://localhost:8080/users/all');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    setProducts(data);
+  } catch (error) {
+    setError(error.message);
+  }
+  setIsLoading(false);
+ };
+
+ useEffect(() => {
+  fetchData();
+ }, []);
+
+ return (
+  <div className="App">
+    <EcommerceNavbar />
+    <EcommerceHeader />
+    <header className="App-header">
+      <h1>Products</h1>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error: {error}</div>
+      ) : (
+        <ProductListing products={products} />
+      )}
+    </header>
+    <EcommerceFooter />
+  </div>
+ );
 }
 
 export default App;
